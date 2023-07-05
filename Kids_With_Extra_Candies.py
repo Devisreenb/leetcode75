5bd9816e16b3a0c38e2b6b8630700bc2a9c26cbb
Devisreenb <devisreenb@gmail.com>
2023-07-05 23:40:13 +0530

    adding extracandies and placeflowers

--- Kids_With_Extra_Candies.py
+++ Kids_With_Extra_Candies.py
@@ -1,6 +1,15 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         res=[]
+        maxi = max(candies)
+        for candy in candies:
+            if candy+extraCandies >= maxi :
+                res.append(True)
+            else:
+                res.append(False)
+        return res
+
+''' bruteforce 
         for i in range(len(candies)):
             candies[i]=candies[i]+extraCandies
             if candies[i] == max(candies) : 
@@ -9,5 +18,7 @@ class Solution:
                 res.append(False)
             candies[i]=candies[i]-extraCandies
         return res
+
+        '''
     
 # TC - O(N)
