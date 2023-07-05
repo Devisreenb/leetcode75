9c6703a0ab981b644dabced4c68f0063a27c528a
Devisreenb <devisreenb@gmail.com>
2023-07-05 22:59:08 +0530

    adding kids with extra candies

---
+++ Kids_With_Extra_Candies.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        res=[]
+        for i in range(len(candies)):
+            candies[i]=candies[i]+extraCandies
+            if candies[i] == max(candies) : 
+                res.append(True)
+            else:
+                res.append(False)
+            candies[i]=candies[i]-extraCandies
+        return res
+    
+# TC - O(N)
